42e3b6080a28dd8ce12d7de95d651de3260fbfae
maheen <maheenzuhra@gmail.com>
2026-02-10 18:30:31 +0500

    complete it

--- app.js
+++ app.js
@@ -1,45 +1,57 @@
-function writeOnly(num) {
-  document.getElementById("input").value += num;
+function writeOnly(cha) {
+  document.getElementById("input").value += cha;
 }
 
 function calculate() {
   var word = document.getElementById("input").value;
-  console.log(word);
-  var ope;
+  var ope = [];
+  var num = [];
   var num1 = "";
   for (var i = 0; i < word.length; i++) {
+    var symbol = word[i];
     if (
-      word[i] === "x" ||
-      word[i] === "+" ||
-      word[i] === "-" ||
-      word[i] === "/" ||
-      word[i] === "%"
+      symbol === "x" ||
+      symbol === "+" ||
+      symbol === "-" ||
+      symbol === "/" ||
+      symbol === "%"
     ) {
-      ope = word[i];
+      num.push(num1);
+      ope.push(symbol);
+      num1 = "";
+    } else {
+      num1 += symbol;
     }
   }
-  var indx = word.indexOf(ope);
-  var num3 = word.slice(0, indx);
-  var num4 = word.slice(indx + 1);
-  var num1 = Number(num3);
-  var num2 = Number(num4);
+  num.push(num1);
   document.getElementById("input").value = "";
-  var result ;
-  if (ope === "+") {
-   result = num1 + num2;
-  } else if (ope === "-") {
-   result = num1 - num2;
-  } else if (ope === "/") {
-    result= num1 / num2;
-  } else if (ope === "%") {
-    result= num1 % num2;
-  } else if (ope === "x") {
-    result= num1 * num2;
-  } else {
-    result= "enetre correct number";
+  var result = Number(num[0]);
+
+  for (var i = 0; i < ope.length; i++) {
+    var num2 = Number(num[i + 1]);
+    //
+    if (ope[i] === "+") {
+      result += num2;
+    } else if (ope[i] === "-") {
+      result -= num2;
+    } else if (ope[i] === "/") {
+      result /= num2;
+    } else if (ope[i] === "%") {
+      result %= num2;
+    } else if (ope[i] === "x") {
+      result *= num2;
+    } else {
+      result = "enetre correct number";
+    }
+    document.getElementById("input").value = result;
   }
-   document.getElementById("input").value = result;
 }
 function clearInput() {
   document.getElementById("input").value = "";
+}
+function dleteCha() {
+  var dWord = document.getElementById("input").value;
+  var dletWord = dWord.length;
+  var finalWord = dWord.slice(0, dletWord - 1);
+  document.getElementById("input").value = finalWord;
 }
